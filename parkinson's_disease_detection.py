a46f4df831ac8cb42ab78ffdc3a090c50f54a4d4
Yuvnish017 <malhotrayuvnish01@gmail.com>
2021-12-04 18:52:19 +0530

    updating codes and adding trained model and prerequisites

---
+++ parkinson's_disease_detection.py
@@ -0,0 +1,315 @@
+"""### Importing the required Libraries and packages"""
+
+import numpy as np
+import cv2
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Dropout, Flatten, Dense, MaxPool2D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.optimizers import Adam, SGD
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+from tensorflow.keras.regularizers import l2
+import tensorflow as tf
+import pandas as pd
+
+"""### Loading Dataset"""
+
+data_train = np.load('dataset/train_set.npz', allow_pickle=True)
+x_train = data_train['arr_0']
+y_train = data_train['arr_1']
+
+print(x_train.shape)
+print(y_train.shape)
+
+data_test = np.load('dataset/test_set.npz', allow_pickle=True)
+x_test = data_test['arr_0']
+y_test = data_test['arr_1']
+
+print(x_test.shape)
+print(y_test.shape)
+
+"""### Data Distribution"""
+
+unique_train, count = np.unique(y_train, return_counts=True)
+plt.figure(figsize=(20, 10))
+sns.barplot(unique_train, count).set_title("Number of training images per category:")
+plt.show()
+
+unique_test, count_test = np.unique(y_test, return_counts=True)
+plt.figure(figsize=(20, 10))
+sns.barplot(unique_test, count_test).set_title("Number of testing images per category:")
+plt.show()
+
+"""As can be seen, though the dataset is balanced but contains very less data points
+
+### Augmenting the Dataset
+"""
+
+train_data_generator = ImageDataGenerator(rotation_range=360, 
+                                    width_shift_range=0.0, 
+                                    height_shift_range=0.0, 
+                                    horizontal_flip=True, 
+                                    vertical_flip=True)
+
+x = list(x_train)
+y = list(y_train)
+
+x_aug_train = []
+y_aug_train = []
+
+for (i, v) in enumerate(y):
+    x_img = x[i]
+    x_img = np.array(x_img)
+    x_img = np.expand_dims(x_img, axis=0)
+    aug_iter = train_data_generator.flow(x_img, batch_size=1, shuffle=True)
+    for j in range(70):
+        aug_image = next(aug_iter)[0].astype('uint8')
+        x_aug_train.append(aug_image)
+        y_aug_train.append(v)
+print(len(x_aug_train))
+print(len(y_aug_train))
+
+x_train = x + x_aug_train
+y_train = y + y_aug_train
+print(len(x_train))
+print(len(y_train))
+
+test_data_generator = ImageDataGenerator(rotation_range=360, 
+                                    width_shift_range=0.0, 
+                                    height_shift_range=0.0, 
+                                    horizontal_flip=True, 
+                                    vertical_flip=True)
+
+x = list(x_test)
+y = list(y_test)
+
+x_aug_test = []
+y_aug_test = []
+
+for (i, v) in enumerate(y):
+    x_img = x[i]
+    x_img = np.array(x_img)
+    x_img = np.expand_dims(x_img, axis=0)
+    aug_iter = test_data_generator.flow(x_img, batch_size=1, shuffle=True)
+    for j in range(20):
+        aug_image = next(aug_iter)[0].astype('uint8')
+        x_aug_test.append(aug_image)
+        y_aug_test.append(v)
+print(len(x_aug_test))
+print(len(y_aug_test))
+
+x_test = x + x_aug_test
+y_test = y + y_aug_test
+print(len(x_test))
+print(len(y_test))
+
+"""### Visualizing the Images in Train and Test Set
+
+##### Images in Train Set
+"""
+
+# print(y_test)
+figure1 = plt.figure(figsize=(5, 5))
+idx_healthy = [i for (i, v) in enumerate(y_train) if v=='healthy']
+img_healthy = x_train[idx_healthy[-1]]
+plt.imshow(img_healthy)
+plt.title('Spiral Drawing by a Healthy Person')
+plt.axis('off')
+plt.show()
+
+figure2 = plt.figure(figsize=(5, 5))
+idx_parkinson = [i for (i, v) in enumerate(y_train) if v=='parkinson']
+img_parkinson = x_train[idx_parkinson[-1]]
+plt.imshow(img_parkinson)
+plt.title("Spiral Drawing by a Person having Parkinson's Disease")
+plt.axis('off')
+plt.show()
+
+"""#### Images in Test Set"""
+
+figure1 = plt.figure(figsize=(5, 5))
+idx_healthy = [i for (i, v) in enumerate(y_test) if v=='healthy']
+img_healthy = x_test[idx_healthy[-1]]
+plt.imshow(img_healthy)
+plt.title('Spiral Drawing by a Healthy Person')
+plt.axis('off')
+plt.show()
+
+figure2 = plt.figure(figsize=(5, 5))
+idx_parkinson = [i for (i, v) in enumerate(y_test) if v=='parkinson']
+img_parkinson = x_test[idx_parkinson[-1]]
+plt.imshow(img_parkinson)
+plt.title("Spiral Drawing by a Person having Parkinson's Disease")
+plt.axis('off')
+plt.show()
+
+"""### Preprocessing the Images"""
+
+for i in range(len(x_train)):
+    img = x_train[i]
+    img = cv2.resize(img, (128, 128))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    x_train[i] = img
+    
+for i in range(len(x_test)):
+    img = x_test[i]
+    img = cv2.resize(img, (128, 128))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    x_test[i] = img
+
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+
+x_train = x_train/255.0
+x_test = x_test/255.0
+
+label_encoder = LabelEncoder()
+y_train = label_encoder.fit_transform(y_train)
+print(y_train.shape)
+
+label_encoder = LabelEncoder()
+y_test = label_encoder.fit_transform(y_test)
+print(y_test.shape)
+
+"""### Data Distribution after Augmentation"""
+
+unique_train, count = np.unique(y_train, return_counts=True)
+plt.figure(figsize=(20, 10))
+sns.barplot(unique_train, count).set_title("Number of training images per category after augmentation:")
+plt.show()
+
+unique_test, count_test = np.unique(y_test, return_counts=True)
+plt.figure(figsize=(20, 10))
+sns.barplot(unique_test, count_test).set_title("Number of test set images per category after augmentation:")
+plt.show()
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+x_train = np.expand_dims(x_train, axis=-1)
+x_test = np.expand_dims(x_test, axis=-1)
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+"""### Defining the Model"""
+
+def parkinson_disease_detection_model(input_shape=(128, 128, 1)):
+    regularizer = tf.keras.regularizers.l2(0.001)
+    model = Sequential()
+    model.add(Input(shape=input_shape))
+    model.add(Conv2D(128, (5, 5), padding='same', strides=(1, 1), name='conv1', activation='relu', 
+                     kernel_initializer='glorot_uniform', kernel_regularizer=regularizer))
+    model.add(MaxPool2D((9, 9), strides=(3, 3)))
+
+    model.add(Conv2D(64, (5, 5), padding='same', strides=(1, 1), name='conv2', activation='relu', 
+                     kernel_initializer='glorot_uniform', kernel_regularizer=regularizer))
+    model.add(MaxPool2D((7, 7), strides=(3, 3)))
+    
+    model.add(Conv2D(32, (3, 3), padding='same', strides=(1, 1), name='conv3', activation='relu', 
+                     kernel_initializer='glorot_uniform', kernel_regularizer=regularizer))
+    model.add(MaxPool2D((5, 5), strides=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), padding='same', strides=(1, 1), name='conv4', activation='relu', 
+                     kernel_initializer='glorot_uniform', kernel_regularizer=regularizer))
+    model.add(MaxPool2D((3, 3), strides=(2, 2)))    
+    
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu', kernel_initializer='glorot_uniform', name='fc1'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax', kernel_initializer='glorot_uniform', name='fc3'))
+    
+    optimizer = Adam(3.15e-5)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+model= parkinson_disease_detection_model(input_shape=(128, 128, 1))
+model.summary()
+
+"""### Training the Model"""
+
+hist = model.fit(x_train, y_train, batch_size=128, epochs=70, validation_data=(x_test, y_test))
+
+"""### Loss and Accuracy Plot"""
+
+figure = plt.figure(figsize=(10, 10))
+plt.plot(hist.history['accuracy'], label='Train_accuracy')
+plt.plot(hist.history['val_accuracy'], label='Test_accuracy')
+plt.title('Model Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc="upper left")
+plt.show()
+
+figure2 = plt.figure(figsize=(10, 10))
+plt.plot(hist.history['loss'], label='Train_loss')
+plt.plot(hist.history['val_loss'], label='Test_loss')
+plt.title('Model Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend(loc="upper left")
+plt.show()
+
+"""### Classification Report"""
+
+ypred = model.predict(x_test)
+ypred = np.argmax(ypred, axis=1)
+y_test_pred = np.argmax(y_test, axis=1)
+print(classification_report(y_test_pred, ypred))
+
+"""### Confusion Matrix"""
+
+matrix = confusion_matrix(y_test_pred, ypred)
+df_cm = pd.DataFrame(matrix, index=[0, 1], columns=[0, 1])
+figure = plt.figure(figsize=(5, 5))
+sns.heatmap(df_cm, annot=True, fmt='d')
+
+"""### Saving the Model"""
+
+model.save('parkinson_disease_detection.h5')
+
+"""### Testing Model on Images"""
+
+labels = ['Healthy', 'Parkinson']
+image_healthy = cv2.imread('dataset/test_image_healthy.png')
+image_parkinson = cv2.imread('dataset/test_image_parkinson.png')
+
+image_healthy = cv2.resize(image_healthy, (128, 128))
+image_healthy = cv2.cvtColor(image_healthy, cv2.COLOR_BGR2GRAY)
+image_healthy = np.array(image_healthy)
+image_healthy = np.expand_dims(image_healthy, axis=0)
+image_healthy = np.expand_dims(image_healthy, axis=-1)
+
+image_parkinson = cv2.resize(image_parkinson, (128, 128))
+image_parkinson = cv2.cvtColor(image_parkinson, cv2.COLOR_BGR2GRAY)
+image_parkinson = np.array(image_parkinson)
+image_parkinson = np.expand_dims(image_parkinson, axis=0)
+image_parkinson = np.expand_dims(image_parkinson, axis=-1)
+
+ypred_healthy = model.predict(image_healthy)
+ypred_parkinson = model.predict(image_parkinson)
+
+figure = plt.figure(figsize=(2, 2))
+img_healthy = np.squeeze(image_healthy, axis=0)
+img_healthy = np.squeeze(img_healthy, axis=-1)
+plt.imshow(img_healthy)
+plt.axis('off')
+plt.title(f'Prediction by the model: {labels[np.argmax(ypred_healthy[0], axis=0)]}')
+plt.show()
+
+figure = plt.figure(figsize=(2, 2))
+image_parkinson = np.squeeze(image_parkinson, axis=0)
+image_parkinson = np.squeeze(image_parkinson, axis=-1)
+plt.imshow(image_parkinson)
+plt.axis('off')
+plt.title(f'Prediction by the model: {labels[np.argmax(ypred_parkinson[0], axis=0)]}')
+plt.show()
